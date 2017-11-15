e33cf3dd3af491261dee6a5c2902db66563753a5
Jean Detoeuf <jean-externe.detoeuf@enedis.fr>
2017-11-15 16:37:13 +0100

    lombok marche

--- src/main/java/com/detoeuf/test/Application.java
+++ src/main/java/com/detoeuf/test/Application.java
@@ -1,12 +1,11 @@
 package com.detoeuf.test;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class Application {
-    private static final Logger logger = LoggerFactory.getLogger("com.detoeuf.test.Application");
 
-    public static void main(String[] args) {
-        logger.info(new Chou().bonjour("Jean"));
-    }
+  public static void main(String[] args) {
+    log.info(new Chou().bonjour("Jean"));
+  }
 }
